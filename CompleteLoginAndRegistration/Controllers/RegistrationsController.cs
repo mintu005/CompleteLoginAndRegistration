3302e3dbb4e7b3c3c33c753e2c17e83128306031
mintu005 <mintu.sumcynet@gmail.com>
2018-09-16 15:28:20 +0600

    CompleteLoginAndRegistration

---
+++ CompleteLoginAndRegistration/Controllers/RegistrationsController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CompleteLoginAndRegistration.Models;
+using System.Net.Mail;
+
+namespace CompleteLoginAndRegistration.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        // Registrations GET Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        // Registrations POST Action
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegistrationForm([Bind(Exclude = "isEmailVarified,ActivationCode")]Registration reg)
+        {
+            bool Status = false;
+            string message = "";
+            //
+            //Model Validation
+            if(ModelState.IsValid)
+            {
+                #region //Email is already exist
+                var isExist = IsEmailExist(reg.Email_ID);
+                if(isExist)
+                {
+                    ModelState.AddModelError("EmailExist", "Email is already exist");
+                    return View(reg);
+                }
+                #endregion
+
+                #region Generate Activation Code
+                reg.ActivationCode = Guid.NewGuid();
+                #endregion
+
+                #region Password Hasing
+                reg.Password = Crypto.Hash(reg.Password);
+                reg.ConfirmPassword = Crypto.Hash(reg.ConfirmPassword); //
+                reg.isEmailVarified = false;
+                #endregion
+
+                #region Save to Database
+                using (LoginRegEntities1 dc = new LoginRegEntities1())
+                {
+                    dc.Registrations.Add(reg);
+                    dc.SaveChanges();
+
+                    //Send Email to User
+                    SendVerificationLinkEmail(reg.Email_ID, reg.ActivationCode.ToString());
+                }
+
+                #endregion
+            }
+            else
+            {
+                message = "Invalid Requiest";
+            }
+            
+            
+          
+          
+            return View(reg);
+        }
+        //Verify Email
+
+        //Verify Email Link
+
+        //Login
+
+        //Login Post
+        [NonAction]
+        public bool IsEmailExist(string emailID)
+        {
+            using (LoginRegEntities1 dc= new LoginRegEntities1())
+            {
+                var v = dc.Registrations.Where(a => a.Email_ID == emailID).FirstOrDefault();
+                return v != null;
+            }
+        }
+
+        [NonAction]
+        public void SendVerificationLinkEmail(string emailID, string activationCode)
+        {
+            var verifyUrl = "/Registration/VerifyAccount" + activationCode;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            var fromEmail = new MailAddress("dotnetawesome@gmail.com", "Dotnet Awesome");
+            var toMail = new MailAddress(emailID);
+            var fromEmailPassword = "**********"; //Replace with Actual Password
+            var subject = "Your Account is successfully created!";
+        }
+
+    }
+}
---
+++ CompleteLoginAndRegistration/Models/Extended/Registration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CompleteLoginAndRegistration.Models.Extended
+{
+    [MetadataType(typeof(RegistrationMetadata))]
+    public partial class Registration
+    {
+        public string Re_Password { get; set; }
+    }
+    public class RegistrationMetadata
+        {[Display(Name = "First Name:")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First Name Required")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name:")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last Name Required")]
+        public string LastName { get; set; }
+
+        [Display(Name = "E-Mail:")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First Name Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email_ID { get; set; }
+
+        [Display(Name = "Date of Birth:")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString ="{0:dd/MM/yyyy}")]
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+
+        [Display(Name = "Enter Password:")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is Required")]
+        [DataType(DataType.Password)]
+        [MinLength(6,ErrorMessage ="Minimum 6 charecter is required")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password:")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Confirm Password is Required")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string Re_Password { get; set; }
+
+        [Display(Name ="Profile Image:")]
+        [Required(ErrorMessage ="Upload Profile Image")]
+        public string ImageFile { get; set; }
+
+        [Display(Name ="Gender:")]
+        [Required(ErrorMessage ="Select the Gender")]
+         public string Gender { get; set; }
+
+
+    }
+}
